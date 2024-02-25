79b8cbd0ff19e2b95c234534be11f45f9a6d3632
Paul-Elian Tabarant <paul-elian.tabarant@octo.com>
2024-02-25 10:42:37 +0100

    wip

--- src/test/java/PremièreItérationAcceptanceTest.java
+++ src/test/java/PremièreItérationAcceptanceTest.java
@@ -6,23 +6,16 @@ import static org.mockito.Mockito.verify;
 public class PremièreItérationAcceptanceTest {
 
     @Test
+    // TODO: interagir directement avec l'entrée standard
     void afficheChiffreAffairesAprèsCréationOffreEtSouscription() {
-        // TODO: interagir directement avec l'entrée standard
-
-        var créerOffre = "offre mensuelle 30";
-
-        var souscrireOffre = "souscrit";
-        var nomPersonne = "Gilles";
-        var identifiantOffre = "mensuelle_30";
-        var consulterChiffreAffairesMensuel = "ca";
-
         var salle = new SalleDeFitness(new OffresEnMémoire(), new SouscriptionsEnMémoire());
         var sortie = mock(Sortie.class);
+
         var commandes = new InterpréteurCommandes(sortie, salle, salle);
 
-        commandes.exécuter(créerOffre);
-        commandes.exécuter(souscrireOffre + " " + nomPersonne + " " + identifiantOffre);
-        commandes.exécuter(consulterChiffreAffairesMensuel);
+        commandes.exécuter("offre mensuelle 30");
+        commandes.exécuter("souscrit Gilles mensuelle_30");
+        commandes.exécuter("ca");
 
         verify(sortie).envoyer("Chiffre d'affaires du mois : 30€");
     }
