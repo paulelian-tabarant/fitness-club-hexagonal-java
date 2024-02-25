5ccacbf32d8dad77bb8c5d167614ad345c9b5427
Paul-Elian Tabarant <paul-elian.tabarant@octo.com>
2024-02-25 10:39:47 +0100

    wip

--- src/main/java/SouscriptionsEnMémoire.java
+++ src/main/java/SouscriptionsEnMémoire.java
@@ -1,6 +1,5 @@
 public class SouscriptionsEnMémoire implements Souscriptions {
     @Override
     public void ajouter(String nomPersonne, String identifiantOffre) {
-        throw new UnsupportedOperationException("Not implemented yet");
     }
 }
