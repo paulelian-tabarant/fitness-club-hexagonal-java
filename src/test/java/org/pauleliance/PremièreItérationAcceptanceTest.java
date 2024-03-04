baa107727864d24ea49da3c4073c57b3d79e9d7e
Paul-Elian Tabarant <paul-elian.tabarant@octo.com>
2024-03-04 18:49:40 +0100

    wip

--- src/test/java/org/pauleliance/PremièreItérationAcceptanceTest.java
+++ src/test/java/org/pauleliance/PremièreItérationAcceptanceTest.java
@@ -14,20 +14,19 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 public class PremièreItérationAcceptanceTest {
-
-    ArgumentCaptor<String> offresCaptor = ArgumentCaptor.captor();
+    SalleDeFitness salle = new SalleDeFitness(new OffresEnMémoire(), new SouscriptionsEnMémoire());
+    Sortie sortie = mock(Sortie.class);
 
     @Test
     // TODO: interagir directement avec l'entrée standard
     void afficheChiffreAffairesAprèsCréationOffreEtSouscription() {
-        var salle = new SalleDeFitness(new OffresEnMémoire(), new SouscriptionsEnMémoire());
-        var sortie = mock(Sortie.class);
-
         var commandes = new CommandesInput(sortie, salle, salle, salle, salle);
 
         commandes.exécuter("offre annuelle_noel2024 30");
         commandes.exécuter("offres");
+
         verify(sortie).envoyer("annuelle_noel2024");
+
         commandes.exécuter("souscrit Gilles annuelle_noel2024");
         // TODO: implémenter la partie server-side des souscriptions
         commandes.exécuter("ca");
