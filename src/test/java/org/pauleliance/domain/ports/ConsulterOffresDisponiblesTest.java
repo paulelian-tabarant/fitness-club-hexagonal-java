5dc8016915cbf1ebbd5bb976d8e74f46d881aba8
Paul-Elian Tabarant <paul-elian.tabarant@octo.com>
2024-03-04 18:08:05 +0100

    wip

--- src/test/java/org/pauleliance/domain/ports/ConsulterOffresDisponiblesTest.java
+++ src/test/java/org/pauleliance/domain/ports/ConsulterOffresDisponiblesTest.java
@@ -10,29 +10,23 @@ import org.pauleliance.domain.ports.serverside.Souscriptions;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class ConsulterOffresDisponiblesTest {
 
-    private Offres offres = mock(Offres.class);
+    private final Offres offres = mock(Offres.class);
 
-    private ConsulterOffresDisponibles consulterOffresDisponibles = new SalleDeFitness(
-            offres,
-            mock(Souscriptions.class)
-    );
+    private final ConsulterOffresDisponibles consulterOffresDisponibles =
+            new SalleDeFitness(offres, mock(Souscriptions.class));
 
     @Test
-    @DisplayName("Donne les identifiants des offres enregistrées")
-    void donneLesIdentifiantsDesOffresEnregistrées() {
-        String offreAnnuelle = "annuelle_noel2024";
-        String offreMensuelle = "mensuelle_noel2024";
+    @DisplayName("Donne les offres enregistrées")
+    void donneLesOffresEnregistrées() {
+        Offre offreAnnuelle = new Offre("annuelle_noel2024", 30);
+        Offre offreMensuelle = new Offre("mensuelle_noel2024", 40);
 
-        when(offres.disponibles()).thenReturn(List.of(
-                new Offre(offreAnnuelle, 30),
-                new Offre(offreMensuelle, 40)
-        ));
+        when(offres.disponibles()).thenReturn(List.of(offreAnnuelle, offreMensuelle));
 
         assertThat(consulterOffresDisponibles.consulterOffresDisponibles()).isEqualTo(
                 List.of(offreAnnuelle, offreMensuelle)
